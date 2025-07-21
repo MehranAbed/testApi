52e2823a40dd6a693296f31ebf388a2197800f84
MehranAbed <Abed@gmail.com>
2025-07-21 13:26:38 +0330

    add fonts and form and style

---
+++ app.js
@@ -0,0 +1,10 @@
+let frm = document.querySelector("form"),
+  metr = document.querySelector(".metr"),
+  zbana = document.querySelector(".zbana"),
+  price = document.querySelector(".price"),
+  sanad = document.querySelector(".sanad"),
+  melk = document.querySelector(".melk label"),
+  waghfi = document.querySelector(".waghfi label"),
+  file = document.querySelector(".file");
+
+  console.log(file.value);
